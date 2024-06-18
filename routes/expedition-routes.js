bd9c514c426b2b226d1c2f42cd0c313ae6977f65
padcoding1 <padcoding@gmail.com>
2024-06-18 02:29:29 -0600

    Removed some console logging...

--- routes/expedition-routes.js
+++ routes/expedition-routes.js
@@ -30,32 +30,3 @@ router.route("/").get(expeditions.renderHome);
 module.exports = router;
 
 ////////////////////////
-/*
-router
-	.route("/")
-	.get(campgrounds.index))
-	.post(
-		isLoggedIn,
-		upload.array("image"),
-		validateSanitizeCampground,
-		campgrounds.createCampground)
-	);
-
-router.get("/new", isLoggedIn, campgrounds.newForm);
-
-router
-	.route("/:id")
-	.get(campgrounds.showCampground))
-	.put(
-		isLoggedIn,
-		upload.array("image"),
-		isOwner,
-		validateSanitizeCampground,
-		campgrounds.editCampground)
-	)
-	.delete(isLoggedIn, isOwner, campgrounds.deleteCampground));
-
-router.get("/:id/edit", isLoggedIn, isOwner, campgrounds.editForm));
-
-module.exports = router;
-*/
